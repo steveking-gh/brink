9565e7d4f66d2cb3759364ea2c06b78fc8162ece
Steve King <sk@metrokings.com>
2020-10-03 23:17:59 -0700

    Added unit tests and parsing.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 #![warn(clippy::all)]
 
 use std::vec::Vec;
-use std::io;
+use std::{io,fs};
 use logos::{Logos};
 use indextree::{Arena,NodeId};
 extern crate clap;
@@ -17,13 +17,10 @@ use codespan_reporting::files::Files;
 use codespan_reporting::term;
 use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
 
-
 pub type Span = std::ops::Range<usize>;
 
 #[derive(Logos, Debug, Clone, PartialEq)]
 pub enum LexToken {
-    _Root_,
-
     #[token("section")] Section,
     #[token("wr")] Wr,
     #[token("{")] OpenBrace,
@@ -95,64 +92,128 @@ pub struct Context<'a> {
     diags: Diags<'a>,
 }
 
-pub fn parse<'toks>(ltv: &'toks[TokenInfo], tn: &mut usize, ctxt: &mut Context) -> Arena<usize> {
-    let mut arena = Arena::new();
-
-    // For uniformity, we want a root node as a parent for recursion
-    // The LexToken index of the root should not get confused with an
-    // actual index into ltv, so use MAX.
-    let mut root = arena.new_node(usize::MAX);
-    let tinfo = &ltv[*tn];
-    println!("Parsing token {}: {:?}", *tn, tinfo);
-    match tinfo.tok {
-        LexToken::Section => parse_section(&mut arena, ltv, tn, &mut root, ctxt),
-        _ => println!("Error"),
+pub fn parse<'toks>(arena : &mut Arena<usize>, ltv: &'toks[TokenInfo],
+                    ctxt: &mut Context) -> bool {
+    let total_toks = ltv.len();
+    if total_toks == 0 {
+        return true;
     }
 
-    arena
+    let mut tok_num = 0;
+    while tok_num < total_toks {
+        let tinfo = &ltv[tok_num];
+        debug!("Parsing token {}: {:?}", &mut tok_num, tinfo);
+        match tinfo.tok {
+            LexToken::Section => {
+                if !parse_section(arena, ltv, &mut tok_num, None, ctxt) {
+                    return false;
+                }
+            }
+            _ => { return false; },
+        }
+    }
+    true
 }
 
 pub fn parse_section<'toks>(arena : &mut Arena<usize>, ltv : &'toks[TokenInfo],
-                            tn : &mut usize, parent : &mut NodeId, ctxt: &mut Context) {
+                            tok_num : &mut usize, parent : Option<NodeId>,
+                            ctxt: &mut Context) -> bool {
 
     // Add the section keyword as a child of the parent
     // All content in the section are children of the section node
-    let mut node = arena.new_node(*tn);
-    parent.append(node, arena);
+    let node = arena.new_node(*tok_num);
+
+    // If the parent exists, attach the child node
+    if let Some(p) = parent {
+        p.append(node, arena);
+    }
 
     // Advance the token number past 'section'
-    *tn += 1;
+    *tok_num += 1;
 
     // After a section declaration, an identifier is expected
-    let tinfo = &ltv[*tn];
+    let tinfo = &ltv[*tok_num];
     if let LexToken::Identifier = tinfo.tok {
-        parse_identifier(arena, tn, &mut node);
+        parse_leaf(arena, tok_num, node);
     } else {
         let diag = Diagnostic::error()
             .with_code("E001")
             .with_message(format!("Expected an identifier after 'section', instead found '{}'", tinfo.slice()))
             .with_labels(vec![Label::primary((), tinfo.span()),
-                              Label::secondary((), ltv[*tn-1].span())]);
-
+                              Label::secondary((), ltv[*tok_num-1].span())]);
         ctxt.diags.emit(&diag);
+        return false;
     }
+
+    // After a section identifier, open brace
+    let tinfo = &ltv[*tok_num];
+    if let LexToken::OpenBrace = tinfo.tok {
+        parse_leaf(arena, tok_num, node);
+    } else {
+        let diag = Diagnostic::error()
+            .with_code("E002")
+            .with_message(format!("Expected '{{' after identifier, instead found '{}'", tinfo.slice()))
+            .with_labels(vec![Label::primary((), tinfo.span()),
+                              Label::secondary((), ltv[*tok_num-1].span())]);
+        ctxt.diags.emit(&diag);
+        return false;
+    }
+
+    parse_section_contents(arena, ltv, tok_num, node, ctxt);
+
+    true
 }
 
-pub fn parse_identifier(arena : &mut Arena<usize>,
-                        tn : &mut usize, parent : &mut NodeId) {
+pub fn parse_section_contents<'toks>(arena : &mut Arena<usize>, ltv : &'toks[TokenInfo],
+                              tok_num : &mut usize, parent : NodeId, ctxt: &mut Context) -> bool {
 
-    // add the identifier as a child of the parent
-    let node = arena.new_node(*tn);
+
+    let total_toks = ltv.len();
+    if total_toks == 0 {
+        return true;
+    }
+
+    while *tok_num < total_toks {
+        let tinfo = &ltv[*tok_num];
+        debug!("Parsing token {}: {:?}", *tok_num, tinfo);
+        match tinfo.tok {
+            // For now, we only support writing strings in a section.
+            LexToken::Wr => parse_leaf(arena, tok_num, parent),
+            LexToken::CloseBrace => {
+                // When we find a close brace, we're done with section content
+                parse_leaf(arena, tok_num, parent);
+                return true;
+            }
+            _ => {
+                let diag = Diagnostic::error()
+                    .with_code("E003")
+                    .with_message(format!("Invalid expression in section '{}'", tinfo.slice()))
+                    .with_labels(vec![Label::primary((), tinfo.span())]);
+                ctxt.diags.emit(&diag);
+                return false;
+            }
+        }
+    }
+    true
+}
+
+/**
+ * Adds the token as a child of teh parent and advances
+ * the token index.
+ */
+pub fn parse_leaf(arena : &mut Arena<usize>,
+        tok_num : &mut usize, parent : NodeId) {
+    let node = arena.new_node(*tok_num);
     parent.append(node, arena);
-
-    // Identifiers are leaf nodes.  Just advance the token index and return.
-    *tn += 1;
+    *tok_num += 1;
 }
 
 /// Entry point for all processing on the input source file
 /// name: The name of the file
 /// fstr: A string containing the file
 pub fn process(name: &str, fstr: &str) {
+    info!("Processing {}", name);
+    debug!("File contains: {}", fstr);
 
     let mut ctxt = Context {
         diags: Diags::new(name,fstr),
@@ -164,14 +225,11 @@ pub fn process(name: &str, fstr: &str) {
         tv.push(TokenInfo{tok: t, s:lex.slice(), loc: lex.span()});
     }
 
-
-    let mut tok_index = 0;
-    let arena = parse(&tv, &mut tok_index, &mut ctxt);
-    for node in arena.iter() {
-        if *node.get() == usize::MAX {
-            continue; // skip the fake root node
-        }
-        println!("Node = {} for token {:?}", *node.get(), tv[*node.get()] );
+    let mut arena = Arena::new();
+    let success = parse(&mut arena, &tv, &mut ctxt);
+    println!("Parsing {}", if success {"succeeded"} else {"failed"});
+    for (node_num, tok_num) in arena.iter().enumerate() {
+        println!("Node {} is token #{} = {}", node_num, *tok_num.get(), tv[*tok_num.get()].s);
     }
 
 }
@@ -213,7 +271,7 @@ fn main() {
             .author(env!("CARGO_PKG_AUTHORS"))
             .about(env!("CARGO_PKG_DESCRIPTION"))
             .arg(Arg::with_name("INPUT")
-                .help("The input specification file.")
+                .help("The input source file.")
                 .required(true)
                 .index(1))
             .arg(Arg::with_name("verbosity")
@@ -244,7 +302,6 @@ fn main() {
     let in_file_name = args.value_of("INPUT")
             .expect("Strange input file argument error.");
 
-            /*
     let result = fs::read_to_string(in_file_name);
     if result.is_err() {
         let e = result.err().unwrap();
@@ -252,9 +309,6 @@ fn main() {
         std::process::exit(-1);
     }
     let in_file = result.unwrap();
-    */
-
-    let in_file = "section {} section foo{/*stu\nff*/92};// line \"quote\" comment\n section bar {0x56};\nsection foo {\"w\\\"o\nw\"}\n\nsection baz {0}";
 
     process(&in_file_name, &in_file);
 }
---
+++ tests/integration.rs
@@ -0,0 +1,114 @@
+
+#[cfg(test)]
+mod tests {
+use assert_cmd::{Command};
+
+#[test]
+fn help_only() {
+    let _cmd = Command::cargo_bin("roust")
+                .unwrap()
+                .arg("--help")
+                .unwrap();
+}
+
+#[test]
+#[should_panic]
+fn missing_input() {
+    let _cmd = Command::cargo_bin("roust")
+                .unwrap()
+                .arg("does_not_exist.roust")
+                .unwrap();
+}
+
+#[test]
+#[should_panic]
+fn no_cli_input() {
+    let _cmd = Command::cargo_bin("roust")
+                .unwrap()
+                .unwrap();
+}
+
+#[test]
+fn empty_1() {
+    let _cmd = Command::cargo_bin("roust")
+                .unwrap()
+                .arg("tests/empty_1.roust")
+                .assert()
+                .failure();
+}
+
+#[test]
+fn empty_2() {
+    let _cmd = Command::cargo_bin("roust")
+                .unwrap()
+                .arg("tests/empty_2.roust")
+                .assert()
+                .failure();
+}
+
+#[test]
+fn line_comment_1() {
+    let _cmd = Command::cargo_bin("roust")
+                .unwrap()
+                .arg("tests/line_comment_1.roust")
+                .assert()
+                .success();
+}
+
+#[test]
+fn line_comment_2() {
+    let _cmd = Command::cargo_bin("roust")
+                .unwrap()
+                .arg("tests/line_comment_2.roust")
+                .assert()
+                .success();
+}
+
+#[test]
+fn multi_comment_1() {
+    let _cmd = Command::cargo_bin("roust")
+                .unwrap()
+                .arg("tests/multi_comment_1.roust")
+                .assert()
+                .success();
+}
+
+#[test]
+fn multi_comment_2() {
+    let _cmd = Command::cargo_bin("roust")
+                .unwrap()
+                .arg("tests/multi_comment_2.roust")
+                .assert()
+                .success();
+}
+
+#[test]
+fn multi_comment_3() {
+    let _cmd = Command::cargo_bin("roust")
+                .unwrap()
+                .arg("tests/multi_comment_3.roust")
+                .assert()
+                .success();
+}
+
+#[test]
+fn simple_section_1() {
+    let _cmd = Command::cargo_bin("roust")
+                .unwrap()
+                .arg("tests/simple_section_1.roust")
+                .assert()
+                .success();
+}
+
+#[test]
+fn section_rename_err_1() {
+    let _cmd = Command::cargo_bin("roust")
+    .unwrap()
+    .arg("tests/section_rename_err_1.roust")
+    .assert()
+    .failure();
+}
+
+
+} // mod tests
+
