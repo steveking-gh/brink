5e85c072345be93d57e8d6f1dcac01a4d8058dc0
Steve King <sk@metrokings.com>
2020-11-24 15:12:53 -0800

    Moved fuzz test to ast.  Works!

--- ast/fuzz/fuzz_targets/fuzz_target_1.rs
+++ ast/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,11 +1,11 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
-use roust::Ast;
+use ast::Ast;
 
 // The fuzzer calls this function repeatedly
 fuzz_target!(|data: &[u8]| {
     if let Ok(str_in) = std::str::from_utf8(data) {
-            let _ = Ast::new("fuzz_test", str_in);
+            let _ = Ast::new(str_in);
     }
 });
